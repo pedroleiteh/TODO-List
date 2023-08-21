8e875ce9b135e03b655bbced1d119958d1908fca
Pedro Leite <contato.pedro77@gmail.com>
2023-08-20 23:04:14 -0300

    add: swagger to api documentation

--- src/main/java/tech/pedroleite/todolistapi/controllers/TaskController.java
+++ src/main/java/tech/pedroleite/todolistapi/controllers/TaskController.java
@@ -1,7 +1,6 @@
 package tech.pedroleite.todolistapi.controllers;
 
 import jakarta.validation.Valid;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
